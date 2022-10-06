9a26bf94cf45d2be75895eed8d1c13fdca1f8c7b
Vladimir Kirillov <vkirillov505@gmail.com>
2022-10-06 22:31:20 +0300

    labs 1,2 update

---
+++ TOKS/Lab2/SerialPortWrapper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Lab2
+{
+    public class SerialPortWrapper
+    {
+        private readonly SerialPort _serialPort;
+
+        public bool Synchronized { get; set; }
+
+        public SerialPortWrapper(string portName, int rate)
+        {
+            _serialPort = new SerialPort(portName, rate, Parity.None, 8, StopBits.One);
+            Synchronized = true;
+        }
+
+        public void InitializePort()
+        {
+            _serialPort.Open();
+
+            if (!_serialPort.IsOpen)
+                throw new NotSupportedException("Cannot initialize port!");
+
+            _serialPort.DataReceived += ReceiveData;
+            _serialPort.ErrorReceived += ReceiveError;
+        }
+
+        public void SendData(string str)
+        {
+            var encoded = PackageEncoder.Encode(str);
+            
+            if (encoded.Length == 0)
+            {
+                Console.WriteLine("No data to send!");
+                return;
+            }
+
+            Console.WriteLine($"{_serialPort.PortName} write: " + encoded);
+
+            _serialPort.Write(encoded.ToCharArray(), 0, encoded.Length);
+
+            if (Synchronized)
+                Thread.Sleep(100);
+        }
+        
+        private void ReceiveData(object sender, SerialDataReceivedEventArgs serialDataReceivedEventArgs)
+        {
+            var data = new char[_serialPort.BytesToRead];
+            _serialPort.Read(data, 0, data.Length);
+
+            Console.Write($"{_serialPort.PortName} read: ");
+            Console.WriteLine(PackageEncoder.Decode(new string(data)));
+        }
+
+        private void SetRate(int rate)
+        {
+            try
+            {
+                _serialPort.BaudRate = rate;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        
+        private static void ReceiveError(object sender, SerialErrorReceivedEventArgs e) =>
+            Console.WriteLine("Receive error!");
+
+        public static void ChangeRate(ref SerialPortWrapper port, int rate)
+        {
+            var currentRate = port._serialPort.BaudRate;
+            port.SetRate(rate);
+            Console.WriteLine($"Baud rate changed from {currentRate} to {rate}");
+        }
+    }
+}
