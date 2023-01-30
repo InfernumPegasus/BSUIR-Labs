9c77b9c1ecb7d6e79c7840011d58955ca5d486fd
Vladimir Kirillov <vkirillov505@gmail.com>
2023-01-30 20:05:28 +0300

    update

--- AVP/Virtualization/main.cpp
+++ AVP/Virtualization/main.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 #include <random>
 #include <chrono>
+//#include <execution>
+#include <emmintrin.h>
+
+#ifdef __SSE2__
+#include <emmintrin.h>
+#else
+#warning SSE2 support is not available. Code will not compile
+#endif
 
 using std::size_t;
 using std::vector;
@@ -31,7 +39,8 @@ public:
     }
 
 public:
-    TMatrix *Multiply(const TMatrix &rhs) {
+    [[gnu::target("no-sse")]]
+    TMatrix *NoVectorizedMultiply(const TMatrix &rhs) {
         if (rhs.rows_ != columns_) {
             throw std::out_of_range("Rows and columns of matrices are not equal!");
         }
@@ -48,27 +57,68 @@ public:
                 cout << "Processing " << i << " of " << rows_ << "\n";
             for (int j{0}; j < rhs.columns_; j++)
                 for (int k{0}; k < rhs.rows_; k++) {
-                    MultiplySubmatrix(matrix_[i][k], rhs.matrix_[k][j], submatrix);
-                    AddSubmatrix(res->matrix_[i][j], submatrix, res->matrix_[i][j]);
+                    NoVectorizedMultiplySubmatrix(matrix_[i][k], rhs.matrix_[k][j], submatrix);
+                    NoVectorizedAddSubmatrix(res->matrix_[i][j], submatrix, res->matrix_[i][j]);
                 }
         }
 
         return res;
     }
 
-    static void MultiplySubmatrix(const Submatrix &m1, const Submatrix &m2, Submatrix &res) {
+    [[gnu::target("no-sse")]]
+    static void NoVectorizedMultiplySubmatrix(const Submatrix &m1, const Submatrix &m2, Submatrix &res) {
         for (int i{0}; i < NESTED_MATRIX_ROWS; i++)
             for (int j{0}; j < NESTED_MATRIX_COLUMNS; j++)
                 for (int k{0}; k < NESTED_MATRIX_ROWS; k++)
                     res[i][j] += m1[i][k] * m2[k][j];
     }
 
-    static void AddSubmatrix(const Submatrix &m1, const Submatrix &m2, Submatrix &res) {
+    [[gnu::target("no-sse")]]
+    static void NoVectorizedAddSubmatrix(const Submatrix &m1, const Submatrix &m2, Submatrix &res) {
         for (int i{0}; i < NESTED_MATRIX_ROWS; i++)
             for (int j{0}; j < NESTED_MATRIX_COLUMNS; j++)
                 res[i][j] = m1[i][j] + m2[i][j];
     }
 
+    TMatrix *AutoVectorizedMultiply(const TMatrix &rhs) {
+        if (rhs.rows_ != columns_) {
+            throw std::out_of_range("Rows and columns of matrices are not equal!");
+        }
+
+        auto *res = new TMatrix(rows_, rhs.columns_);
+
+        Submatrix submatrix(NESTED_MATRIX_ROWS);
+        for (auto &nRow: submatrix) {
+            nRow.resize(NESTED_MATRIX_COLUMNS);
+        }
+
+        for (int i{0}; i < rows_; i++) {
+            if (i % 100 == 0)
+                cout << "Processing " << i << " of " << rows_ << "\n";
+            for (int j{0}; j < rhs.columns_; j++)
+                for (int k{0}; k < rhs.rows_; k++) {
+                    AutoVectorizedMultiplySubmatrix(matrix_[i][k], rhs.matrix_[k][j], submatrix);
+                    AutoVectorizedAddSubmatrix(res->matrix_[i][j], submatrix, res->matrix_[i][j]);
+                }
+        }
+
+        return res;
+    }
+
+    static void AutoVectorizedMultiplySubmatrix(const Submatrix &m1, const Submatrix &m2, Submatrix &res) {
+        for (int i{0}; i < NESTED_MATRIX_ROWS; i++)
+            for (int j{0}; j < NESTED_MATRIX_COLUMNS; j++)
+                for (int k{0}; k < NESTED_MATRIX_ROWS; k++)
+                    res[i][j] += m1[i][k] * m2[k][j];
+    }
+
+    static void AutoVectorizedAddSubmatrix(const Submatrix &m1, const Submatrix &m2, Submatrix &res) {
+        for (int i{0}; i < NESTED_MATRIX_ROWS; i++)
+            for (int j{0}; j < NESTED_MATRIX_COLUMNS; j++)
+                res[i][j] = m1[i][j] + m2[i][j];
+    }
+
+public:
     bool operator==(const TMatrix &rhs) const {
         if (rows_ != rhs.rows_ ||
             columns_ != rhs.columns_) {
@@ -77,7 +127,6 @@ public:
         return matrix_ == rhs.matrix_;
     }
 
-public:
     [[nodiscard]] size_t GetRows() const {
         return rows_;
     }
@@ -130,28 +179,108 @@ private:
     Matrix matrix_;
 };
 
+int N = 5;
+void mult(int **mat1, int **mat2, int **result) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j += 8) {
+            auto sum = _mm_setzero_si128();
+            for (int k = 0; k < N; k++) {
+                auto bc_mat1 = _mm_set1_epi32(mat1[i][k]);
+                auto vec_mat2 = _mm_loadu_si16((__m128i*)&mat2[k][j]);
+                auto prod = _mm_mullo_epi16(bc_mat1, vec_mat2);
+                sum = _mm_add_epi32(sum, prod);
+            }
+            _mm_storeu_si16((__m128i*)&result[i][j], sum);
+        }
+    }
+}
+
 int main() {
+//    {
+//        TMatrix tMatrix1(300, 300);
+//        TMatrix tMatrix2(300, 300);
+//
+//        cout << "T1: " << tMatrix1.GetRows() << " " << tMatrix1.GetColumns() << "\n";
+//        tMatrix1.FillRandom();
+////        tMatrix1.Print();
+//        cout << "T2: " << tMatrix2.GetRows() << " " << tMatrix2.GetColumns() << "\n";
+//        tMatrix2.FillRandom();
+////        tMatrix2.Print();
+//
+//        auto start = std::chrono::high_resolution_clock::now();
+//        auto multiplied = tMatrix1.AutoVectorizedMultiply(tMatrix2);
+//        auto end = std::chrono::high_resolution_clock::now();
+//        auto dif = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+//        std::cout << "Time : " << dif << "\n";
+//
+//        cout << "T3: " << multiplied->GetRows() << " " << multiplied->GetColumns() << "\n";
+////        multiplied->Print();
+//
+//        start = std::chrono::high_resolution_clock::now();
+//        multiplied = tMatrix1.NoVectorizedMultiply(tMatrix2);
+//        end = std::chrono::high_resolution_clock::now();
+//        dif = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+//        std::cout << "Time : " << dif << "\n";
+//
+//        cout << "T3: " << multiplied->GetRows() << " " << multiplied->GetColumns() << "\n";
+//
+//        delete multiplied;
+//    }
+
     {
-        TMatrix tMatrix1(1000, 1000);
-        TMatrix tMatrix2(1000, 1000);
+        int **array1 = new int *[N];
+        for (int i = 0; i < N; i++) {
+            array1[i] = new int[N];
+        }
+        int **array2 = new int *[N];
+        for (int i = 0; i < N; i++) {
+            array2[i] = new int[N];
+        }
+        int **array3 = new int *[N];
+        for (int i = 0; i < N; i++) {
+            array3[i] = new int[N];
+        }
 
-        cout << "T1: " << tMatrix1.GetRows() << " " << tMatrix1.GetColumns() << "\n";
-        tMatrix1.FillRandom();
-//        tMatrix1.Print();
-        cout << "T2: " << tMatrix2.GetRows() << " " << tMatrix2.GetColumns() << "\n";
-        tMatrix2.FillRandom();
-//        tMatrix2.Print();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                array1[i][j] = 1;
+                array2[i][j] = 2;
+//                array3[i][j] = 3;
+            }
+        }
 
-        auto start = std::chrono::high_resolution_clock::now();
-        auto multiplied = tMatrix1.Multiply(tMatrix2);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto dif = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
-        std::cout << "Time : " << dif << "\n";
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << array1[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
 
-        cout << "T3: " << multiplied->GetRows() << " " << multiplied->GetColumns() << "\n";
-//        multiplied->Print();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << array2[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
 
-        delete multiplied;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << array3[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+
+        mult(array1, array2, array3);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << array3[i][j] << "\t";
+            }
+            cout << "\n";
+        }
     }
 
     return 0;
