b73a686c088408bb8ff1e4bbf90c5410ca3cfac7
Vladimir Kirillov <vkirillov505@gmail.com>
2024-02-01 12:59:14 +0300

    upd

--- SPOLKS/8-sem/Lab7/main.cpp
+++ SPOLKS/8-sem/Lab7/main.cpp
@@ -1,24 +1,38 @@
 #include <mpi.h>
 
+#include <functional>
 #include <iostream>
 #include <random>
 #include <source_location>
 
-#define N 5
-#define X 5
-#define Y 5
+#define N 4
+#define X 4
+#define Y 4
 
-template <typename T, typename U>
-void AssertEquals(T expressionResult, U expectedResult,
-                  const std::source_location& loc = std::source_location::current()) {
-  if (expressionResult != expectedResult) {
-    std::cerr << "Assertion failed: " << loc.file_name() << " at line " << loc.line()
+void AssertMPISuccess(int expressionResult) {
+  if (expressionResult != MPI_SUCCESS) {
+    const std::source_location& loc = std::source_location::current();
+    std::cerr << "Assertion failed: " << std::source_location::current().file_name() << " at line " << loc.line()
               << std::endl;
-    std::abort();
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
   }
 }
 
-void FillRandom(int **vec, size_t n, size_t m) {
+void AllocateMatrix(int**& matrix, int n, int m) {
+  matrix = new int*[n];
+  for (int i = 0; i < n; ++i) {
+    matrix[i] = new int[m];
+  }
+}
+
+void DeallocateMatrix(int**& matrix, int n) {
+  for (int i = 0; i < n; ++i) {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
+}
+
+void FillRandom(int** vec, size_t n, size_t m) {
   static std::random_device rd;
   static std::mt19937 gen(rd());
   static std::uniform_int_distribution<> distrib(1, 255);
@@ -30,36 +44,25 @@ void FillRandom(int **vec, size_t n, size_t m) {
   }
 }
 
-int main(int argc, char** argv) {
-  int size, rank, sum = 0, i, j, k;
-  int** matrix1 = new int*[X]; // matrix1[X][N]
-  int** matrix2 = new int*[N]; // matrix2[N][Y]
-  int** mat_res = new int*[X]; // mat_res[X][Y]
-  double t1, t2, result;
+void PrintMatrix(int* const* matrix, int n, int m) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      std::cout << matrix[i][j] << "\t";  // print the result
+    }
+    std::cout << std::endl;
+  }
+}
+
+void CalculateSynchronized(int size, int rank, int* const* matrix1, int* const* matrix2,
+                           int** mat_res) {
+  int sum = 0;
+  double result;
   MPI_Status status;
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  for (i = 0; i < X; i++) {
-    matrix1[i] = new int[N];
-    mat_res[i] = new int[Y];
-  }
-
-  for (i = 0; i < N; i++) {
-    matrix2[i] = new int[Y];
-  }
-
-  FillRandom(matrix1, X, N); // Initialize matrix1 with value 1
-  FillRandom(matrix2, N, Y); // Initialize matrix2 with value 2
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  if (rank == 0) t1 = MPI_Wtime();
-  for (i = rank; i < X; i = i + size) { // divide the task in multiple processes
-    for (j = 0; j < Y; j++) {
+  for (int i = rank; i < X; i = i + size) {  // divide the task in multiple processes
+    for (int j = 0; j < Y; j++) {
       sum = 0;
-      for (k = 0; k < N; k++) {
+      for (int k = 0; k < N; k++) {
         sum = sum + matrix1[i][k] * matrix2[k][j];
       }
       mat_res[i][j] = sum;
@@ -67,44 +70,66 @@ int main(int argc, char** argv) {
   }
 
   if (rank != 0) {
-    for (i = rank; i < X; i = i + size)
+    for (int i = rank; i < X; i = i + size)
       MPI_Send(&mat_res[i][0], Y, MPI_INT, 0, 10 + i,
-               MPI_COMM_WORLD); // send calculated rows to process with rank 0
+               MPI_COMM_WORLD);  // send calculated rows to process with rank 0
   }
 
   if (rank == 0) {
-    for (j = 1; j < size; j++) {
-      for (i = j; i < X; i = i + size) {
+    for (int j = 1; j < size; j++) {
+      for (int i = j; i < X; i = i + size) {
         MPI_Recv(&mat_res[i][0], Y, MPI_INT, j, 10 + i, MPI_COMM_WORLD,
-                 &status); // receive calculated rows from respective process
+                 &status);  // receive calculated rows from respective process
       }
     }
   }
   MPI_Barrier(MPI_COMM_WORLD);
-  if (rank == 0) t2 = MPI_Wtime();
-  result = t2 - t1;
+}
+
+template <typename Func>
+constexpr void CalculateTime(Func f, int size, int rank, int* const* matrix1,
+                             int* const* matrix2, int** mat_res) {
+  f(size, rank, matrix1, matrix2, mat_res);
+}
+
+int main(int argc, char** argv) {
+  int size, rank;
+
+  int** matrix1;
+  int** matrix2;
+  int** mat_res;
+
+  AllocateMatrix(matrix1, X, N);
+  AllocateMatrix(matrix2, N, Y);
+  AllocateMatrix(mat_res, X, Y);
+
+  FillRandom(matrix1, X, N);
+  FillRandom(matrix2, N, Y);
+
+  AssertMPISuccess(MPI_Init(&argc, &argv));
+  AssertMPISuccess(MPI_Comm_size(MPI_COMM_WORLD, &size));
+  AssertMPISuccess(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+
+  // BARRIER USAGE
+  double t1;
+  AssertMPISuccess(MPI_Barrier(MPI_COMM_WORLD));
+  if (rank == 0) t1 = MPI_Wtime();
+
+  CalculateTime(CalculateSynchronized, size, rank, matrix1, matrix2, mat_res);
+
   if (rank == 0) {
-    for (i = 0; i < X; i++) {
-      for (j = 0; j < Y; j++) {
-        std::cout << mat_res[i][j] << "\t"; // print the result
-      }
-      std::cout << std::endl;
-    }
-  }
-  if (rank == 0) std::cout << "Time taken = " << result << " seconds\n"; // time taken
+    const auto t2 = MPI_Wtime();
+    const auto result = t2 - t1;
 
-  for (i = 0; i < X; i++) {
-    delete[] matrix1[i];
-    delete[] mat_res[i];
+    PrintMatrix(mat_res, X, Y);
+    std::cout << "Time taken = " << result << " seconds\n";
   }
-  delete[] matrix1;
-  delete[] mat_res;
 
-  for (i = 0; i < N; i++) {
-    delete[] matrix2[i];
-  }
-  delete[] matrix2;
+  AssertMPISuccess(MPI_Finalize());
 
-  MPI_Finalize();
+  DeallocateMatrix(matrix1, X);
+  DeallocateMatrix(mat_res, X);
+  DeallocateMatrix(matrix2, N);
+
   return 0;
 }
