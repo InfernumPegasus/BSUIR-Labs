0b9b34b83e28d17559033fb4665ae644ec905ec6
Vladimir Kirillov <vkirillov505@gmail.com>
2024-02-15 18:26:05 +0300

    upd for mpich

--- SPOLKS/8-sem/Lab7/Matrix.h
+++ SPOLKS/8-sem/Lab7/Matrix.h
@@ -6,52 +6,43 @@
 #include <memory>
 #include <random>
 
-template <size_t Size>
+template<size_t Size>
 class Matrix final {
- private:
-  int (*matrix)[Size];
+public:
+    int (*matrix)[Size];
 
- public:
-  constexpr Matrix() { matrix = new int[Size][Size]; }
+public:
+    constexpr Matrix() { matrix = new int[Size][Size]; }
 
-  constexpr ~Matrix() { delete[] matrix; }
+    constexpr ~Matrix() { delete[] matrix; }
 
-  constexpr void Print() const noexcept {
-    for (size_t i = 0; i < Size; i++) {
-      for (size_t j = 0; j < Size; j++) {
-        std::cout << std::setw(5) << matrix[i][j];
-      }
-      std::cout << '\n';
+    void Randomize() noexcept {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        static std::uniform_int_distribution<> distrib(1, 10);
+
+        for (size_t i = 0; i < Size; i++) {
+            for (size_t j = 0; j < Size; j++) {
+                matrix[i][j] = distrib(gen);
+            }
+        }
     }
-  }
 
-  void Randomize() noexcept {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<> distrib(1, 10);
+    [[nodiscard]] constexpr auto PlainArray() noexcept { return matrix; }
+    [[nodiscard]] constexpr auto PlainArray() const noexcept { return matrix; }
 
-    for (size_t i = 0; i < Size; i++) {
-      for (size_t j = 0; j < Size; j++) {
-        matrix[i][j] = distrib(gen);
-      }
+    constexpr auto operator[](size_t i) const noexcept { return *(matrix + i); }
+    constexpr auto operator[](size_t i) noexcept { return *(matrix + i); }
+
+    template<size_t N>
+    constexpr friend bool operator==(const Matrix<N> &m1,
+                                     const Matrix<N> &m2) noexcept {
+        for (size_t i = 0; i < N; i++)
+            for (size_t j = 0; j < N; j++)
+                if (m1[i][j] != m2[i][j]) return false;
+
+        return true;
     }
-  }
-
-  [[nodiscard]] constexpr auto PlainArray() noexcept { return matrix; }
-  [[nodiscard]] constexpr auto PlainArray() const noexcept { return matrix; }
-
-  constexpr auto operator[](size_t i) const noexcept { return *(matrix + i); }
-  constexpr auto operator[](size_t i) noexcept { return *(matrix + i); }
-
-  template<size_t N>
-  constexpr friend bool operator==(const Matrix<N>& m1,
-                                   const Matrix<N>& m2) noexcept {
-    for (size_t i = 0; i < N; i++)
-      for (size_t j = 0; j < N; j++)
-        if (m1[i][j] != m2[i][j]) return false;
-
-    return true;
-  }
 };
 
-#endif  // LAB7_MATRIX_H
+#endif// LAB7_MATRIX_H
--- SPOLKS/8-sem/Lab7/main.cpp
+++ SPOLKS/8-sem/Lab7/main.cpp
@@ -6,135 +6,136 @@
 
 #include "Matrix.h"
 
-#define MPI_CHECK(call)                                                            \
-  do {                                                                             \
-    const int result = call;                                                       \
-    if (result != MPI_SUCCESS) {                                                   \
-      static std::string error_string;                                             \
-      static int length;                                                           \
-      MPI_Error_string(result, error_string.data(), &length);                      \
-      const auto loc = std::source_location::current();                            \
-      std::cerr << "MPI error at " << loc.file_name() << ":" << loc.line() << ": " \
-                << error_string << std::endl;                                      \
-      MPI_Abort(MPI_COMM_WORLD, result);                                           \
-    }                                                                              \
-  } while (0)
+#define MPI_CHECK(call)                                                                  \
+    do {                                                                                 \
+        const int result = call;                                                         \
+        if (result != MPI_SUCCESS) {                                                     \
+            static std::string error_string;                                             \
+            static int length;                                                           \
+            MPI_Error_string(result, error_string.data(), &length);                      \
+            const auto loc = std::source_location::current();                            \
+            std::cerr << "MPI error at " << loc.file_name() << ":" << loc.line() << ": " \
+                      << error_string << std::endl;                                      \
+            MPI_Abort(MPI_COMM_WORLD, result);                                           \
+        }                                                                                \
+    } while (0)
 
-template <size_t Size>
-constexpr double PerformMatrixMultiplication(const Matrix<Size>& A, const Matrix<Size>& B,
-                                             Matrix<Size>& C, const int rank,
+template<size_t Size>
+constexpr double PerformMatrixMultiplication(const Matrix<Size> &A, const Matrix<Size> &B,
+                                             Matrix<Size> &C, const int rank,
                                              const int processors,
                                              const bool synchronous) {
-  // Calculate the range of rows to be processed by this rank
-  const int from = rank * Size / processors;
-  const int to = (rank + 1) * Size / processors;
-  constexpr auto SizeSquare = Size * Size;
+    // Calculate the range of rows to be processed by this rank
+    const int from = rank * Size / processors;
+    const int to = (rank + 1) * Size / processors;
+    constexpr size_t SizeSquare = Size * Size;
 
-  // Measure the start time
-  const auto start = MPI_Wtime();
+    Matrix<Size> resultMatrix;
+    Matrix<Size> resultA;
+    Matrix<Size> resultB;
 
-  // Perform matrix multiplication
-  if (synchronous) {
-    // Synchronous mode: broadcast matrix B from process 0 and scatter matrix A to all
-    // processes
-    MPI_CHECK(MPI_Bcast(B.PlainArray(), SizeSquare, MPI_INT, 0, MPI_COMM_WORLD));
-    MPI_CHECK(MPI_Scatter(A.PlainArray(), SizeSquare / processors, MPI_INT, A[from],
-                          SizeSquare / processors, MPI_INT, 0, MPI_COMM_WORLD));
-  } else {
-    // Asynchronous mode: non-blocking broadcast of matrix B and scatter of matrix A
-    MPI_Request request[2];
-    MPI_Status status[2];
+    // Measure the start time
+    const auto start = MPI_Wtime();
 
-    MPI_CHECK(
-        MPI_Ibcast(B.PlainArray(), SizeSquare, MPI_INT, 0, MPI_COMM_WORLD, &request[0]));
-    MPI_CHECK(MPI_Iscatter(A.PlainArray(), SizeSquare / processors, MPI_INT, A[from],
-                           SizeSquare / processors, MPI_INT, 0, MPI_COMM_WORLD,
-                           &request[1]));
+    // Perform matrix multiplication
+    if (synchronous) {
+        // Synchronous mode: broadcast matrix B from process 0 and scatter matrix A to all
+        // processes
+        MPI_CHECK(MPI_Bcast(B.PlainArray(), SizeSquare, MPI_INT, 0, MPI_COMM_WORLD));
+        MPI_CHECK(MPI_Scatter(A.PlainArray(), SizeSquare / processors, MPI_INT, resultA[from],
+                              SizeSquare / processors, MPI_INT, 0, MPI_COMM_WORLD));
+    } else {
+        // Asynchronous mode: non-blocking broadcast of matrix B and scatter of matrix A
+        MPI_Request request[2];
+        MPI_Status status[2];
 
-    // Wait for the communication operations to complete
-    MPI_CHECK(MPI_Waitall(2, request, status));
-  }
+        MPI_CHECK(
+                MPI_Ibcast(B.PlainArray(), SizeSquare, MPI_INT, 0, MPI_COMM_WORLD, &request[0]));
+        MPI_CHECK(MPI_Iscatter(A.PlainArray(), SizeSquare / processors, MPI_INT, resultA[from],
+                               SizeSquare / processors, MPI_INT, 0, MPI_COMM_WORLD,
+                               &request[1]));
 
-  // Perform the actual matrix multiplication for the assigned rows
-  for (int i = from; i < to; i++) {
-    for (size_t j = 0; j < Size; j++) {
-      C[i][j] = 0;
-      for (size_t k = 0; k < Size; k++) {
-        C[i][j] += A[i][k] * B[k][j];
-      }
+        // Wait for the communication operations to complete
+        MPI_CHECK(MPI_Waitall(2, request, status));
     }
-  }
 
-  // Gather the results from all processes to process 0
-  MPI_CHECK(MPI_Gather(C.PlainArray()[from], SizeSquare / processors, MPI_INT,
-                       C.PlainArray(), SizeSquare / processors, MPI_INT, 0,
-                       MPI_COMM_WORLD));
+    // Perform the actual matrix multiplication for the assigned rows
+    for (int i = from; i < to; i++) {
+        for (size_t j = 0; j < Size; j++) {
+            resultMatrix[i][j] = 0;
+            for (size_t k = 0; k < Size; k++) {
+                resultMatrix[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
 
-  double totalTime = 0.0;
-  // Measure the total time and print the result only in process 0
-  if (rank == 0) {
-    totalTime = MPI_Wtime() - start;
-    std::cout << "Result:\n";
-    C.Print();
-  }
+    // Gather the results from all processes to process 0
+    MPI_CHECK(MPI_Gather(C.PlainArray()[from], SizeSquare / processors, MPI_INT,
+                         resultMatrix.PlainArray(), SizeSquare / processors, MPI_INT, 0,
+                         MPI_COMM_WORLD));
 
-  return totalTime;
+    double totalTime = 0.0;
+    // Measure the total time and print the result only in process 0
+    if (rank == 0) {
+        totalTime = MPI_Wtime() - start;
+    }
+
+    for (size_t i = 0; i < Size; i++) {
+        for (size_t j = 0; j < Size; j++) {
+            C[i][j] = resultMatrix[i][j];
+        }
+    }
+
+    return totalTime;
 }
 
-int main(int argc, char* argv[]) {
-  constexpr auto MATRIX_SIZE = 3;
+int main(int argc, char *argv[]) {
+    constexpr auto MATRIX_SIZE = 600;
 
-  Matrix<MATRIX_SIZE> A{};
-  Matrix<MATRIX_SIZE> B{};
-  Matrix<MATRIX_SIZE> C_sync{};
-  Matrix<MATRIX_SIZE> C_async{};
+    Matrix<MATRIX_SIZE> A{};
+    Matrix<MATRIX_SIZE> B{};
+    Matrix<MATRIX_SIZE> C_sync{};
+    Matrix<MATRIX_SIZE> C_async{};
 
-  A.Randomize();
-  B.Randomize();
+    A.Randomize();
+    B.Randomize();
 
-  int rank{}, size{};
+    int rank{}, size{};
 
-  MPI_CHECK(MPI_Init(&argc, &argv));
-  MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank)); /* who am i */
-  MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &size)); /* number of processors */
+    MPI_CHECK(MPI_Init(&argc, &argv));
+    MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank)); /* who am i */
+    MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &size)); /* number of processors */
 
-  if (rank == 0) {
-    std::cout << "A:\n";
-    A.Print();
-    std::cout << "B:\n";
-    B.Print();
-  }
+    // Check if matrix size is divisible by the number of processes
+    if (MATRIX_SIZE % size != 0) {
+        if (rank == 0) std::cout << "Matrix size not divisible by number of size\n";
+        MPI_CHECK(MPI_Finalize());
+        MPI_CHECK(MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE));
+    }
 
-  // Check if matrix size is divisible by the number of processes
-  if (MATRIX_SIZE % size != 0) {
-    if (rank == 0) std::cout << "Matrix size not divisible by number of size\n";
+    // Calculate synchronized matrix multiplication
+    double timeSync = PerformMatrixMultiplication(A, B, C_sync, rank, size, true);
+
+    // Calculate asynchronous matrix multiplication
+    double timeAsync = PerformMatrixMultiplication(A, B, C_async, rank, size, false);
+
     MPI_CHECK(MPI_Finalize());
-    MPI_CHECK(MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE));
-  }
 
-  // Calculate synchronized matrix multiplication
-  double timeSync = PerformMatrixMultiplication(A, B, C_sync, rank, size, true);
+    // Print the total time and comparison result
+    if (rank == 0) {
+        std::cout << "\nTotal time (CalculateSynchronized): " << std::fixed
+                  << std::setprecision(6) << timeSync << " (sec)\n";
+        std::cout << "Total time (CalculateAsynchronous): " << std::fixed
+                  << std::setprecision(6) << timeAsync << " (sec)\n";
 
-  // Calculate asynchronous matrix multiplication
-  double timeAsync = PerformMatrixMultiplication(A, B, C_async, rank, size, false);
+        const auto equal = C_sync == C_async;
+        std::cout << "Matrices equal: " << std::boolalpha << equal << std::noboolalpha
+                  << "\n\n";
 
-  MPI_CHECK(MPI_Finalize());
+        const auto syncToAsyncTimeCoefficient = timeSync / timeAsync;
+        std::cout << "Sync mode is slower then Async by " << syncToAsyncTimeCoefficient
+                  << " times\n";
+    }
 
-  // Print the total time and comparison result
-  if (rank == 0) {
-    std::cout << "\nTotal time (CalculateSynchronized): " << std::fixed
-              << std::setprecision(6) << timeSync << " (sec)\n";
-    std::cout << "Total time (CalculateAsynchronous): " << std::fixed
-              << std::setprecision(6) << timeAsync << " (sec)\n";
-
-    const auto equal = C_sync == C_async;
-    std::cout << "Matrices equal: " << std::boolalpha << equal << std::noboolalpha
-              << "\n\n";
-
-    const auto syncToAsyncTimeCoefficient = timeSync / timeAsync;
-    std::cout << "Sync mode is slower then Async by " << syncToAsyncTimeCoefficient
-              << " times\n";
-  }
-
-  return 0;
+    return 0;
 }
